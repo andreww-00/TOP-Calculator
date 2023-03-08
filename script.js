57c7b510f9541736038a4ca72ae205c21dcd55c5
Andrew <a.lucas.webster@gmail.com>
2023-03-08 14:12:27 -0500

    consolidated arithamtic functions into an operate function

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-function add(a, b) {
-    return a+b;
-}
+function operate(operator, a, b) {
+    if (operator === '+') {
+        return a+b;
+    } else if (operator === '-') {
+        return a-b;
+    } else if (operator === '*') {
+        return a*b;
+    } else if (operator === '/') {
+        return a/b;
+    }
 
-function sub(a, b) {
-    return a-b;
-}
-
-function mult(a, b) {
-    return a*b;
-}
-
-function div(a, b) {
-    return a/b;
 }
