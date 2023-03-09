f3fcda6335d9dec6d97e8731bc63c03d1d3f4e9d
Andrew <a.lucas.webster@gmail.com>
2023-03-09 14:44:33 -0500

    added variables that store numbers and operators

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = 0; 
+
 function operate(operator, a, b) {
     if (operator === '+') {
         return a+b;
@@ -16,11 +20,16 @@ let buttonArray = [...button];
 let displayValue = []
 buttonArray.forEach((item) => {
     item.addEventListener("click", (e) => {
+        if (item.getAttribute("class") == "operator") {
+            operator = item.value;
+        } else {
         displayValue.push(item.value);
-        let number = displayValue.join("");
-        display(number);
+        firstNumber = displayValue.join("");
+        display(firstNumber);
+        }
     });
 });
+
 
 
 function display(number) {
@@ -28,3 +37,12 @@ function display(number) {
     display.textContent = number; 
     document.body.div.appendChild(display);
 }
+
+//run through of logic
+
+//first number is entered and saved
+//operated is assigned and saved
+//second number is entered, 
+    //this should update the text on the calculator
+    // value is also saved
+// pressing enter runs the operation and returns a value
