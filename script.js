5bf4347e35dadb62f9e14b4696aee4d48eaffb3b
Andrew <a.lucas.webster@gmail.com>
2023-03-09 11:30:02 -0500

    added button click for numbers

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function operate(operator, a, b) {
     }
 
 }
+
+let button = document.querySelectorAll("button");
+let buttonArray = [...button];
+let displayValue = []
+buttonArray.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        displayValue.push(item.value);
+        let number = displayValue.join("");
+        display(number);
+    });
+});
+
+
+function display(number) {
+    const display = document.querySelector(".display");
+    display.textContent = number; 
+    document.body.div.appendChild(display);
+}
